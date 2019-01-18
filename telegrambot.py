b93e6a4afd7b509d582c1525944f57029ac2e8c3
Максим Распопов <maxim.raspopov@gmail.com>
2019-01-18 07:34:01 +0300

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -7,7 +7,7 @@ from requests.auth import HTTPDigestAuth
 class teleBot:
     def __init__(self, token):
         self.auth = HTTPDigestAuth("raspopovmt", "N7c5LaW")
-        self.proxie = "socks5://181.214.112.2:65234"
+        self.proxie = "http://181.214.112.2:65234"
         self.token = token
         self.api_url = "https://api.telegram.org/bot{}/".format(
             token)
@@ -15,14 +15,14 @@ class teleBot:
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params, proxies = {"socks5" : self.proxie})
+        resp = requests.get(self.api_url + method, params, proxies = self.proxie)
         result_json = resp.json()['result']
         return result_json
 
     def send_message(self, chat_id, text):
         params = {'chat_id': chat_id, 'text': text}
         method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params, proxies = {"socks5" : self.proxie})
+        resp = requests.post(self.api_url + method, params, proxies = self.proxie)
         return resp
 
     def get_last_update(self):
